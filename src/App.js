10f8807967ec5629e0a8856a951b0f3686c6169a
Dora Belme <contact.dorabelme@gmail.com>
2019-07-17 14:40:09 -0700

    added Header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, {useState, useEffect} from "react";
 import "./App.css";
 import Card from "./components/Card";
+import Header from "./components/Header";
 
 
 function App() {
 
   return (
     <div className="App">
+      <Header mainHeader="NASA Photo of the Day 🚀" />
       <Card description="Description" />
       {/* <p>
         Read through the instructions in the README.md file to build your NASA
